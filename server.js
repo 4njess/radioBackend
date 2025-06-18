60cfe1b36637841240a047e45e0defd6fdad4fe9
4njess <artemnfedov777@gmail.com>
2025-06-19 01:11:02 +0500

    as

--- server.js
+++ server.js
@@ -20,36 +20,24 @@ const allowedOrigins = [
     "https://radiobackend-iss7.onrender.com",
 ];
 
-app.options('*', (req, res) => {
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Range');
-        res.setHeader('Access-Control-Expose-Headers', 'Content-Length, Content-Range');
-        res.setHeader('Access-Control-Allow-Credentials', 'true');
-    }
-    res.sendStatus(204);
-});
+
 // Настройка CORS
-app.use((req, res, next) => {
-    const origin = req.headers.origin;
-    
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Range');
-        res.setHeader('Access-Control-Expose-Headers', 'Content-Length, Content-Range');
-        res.setHeader('Access-Control-Allow-Credentials', 'true');
-        
-        // Обрабатываем preflight запросы
-        if (req.method === 'OPTIONS') {
-            return res.sendStatus(204);
-        }
-    }
-    
-    next();
-});
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                console.error(`Blocked by CORS: ${origin}`);
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization", "Range"],
+        exposedHeaders: ["Content-Length", "Content-Range"],
+        credentials: true,
+    })
+);
 
 // Тестовые эндпоинты
 app.get("/", (req, res) => {
